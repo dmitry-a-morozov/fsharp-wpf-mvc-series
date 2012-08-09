27f9fe74cbde581f225c8969f8f0152841949ff0
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-08-09 19:52:05 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@91 aa69542f-e842-d75e-ec41-f0a99e374f1c

---
+++ Controller/BasicController/Sample.fs
@@ -0,0 +1,60 @@
+﻿
+namespace Mvc.Wpf.Sample
+
+open Mvc.Wpf
+open System
+open System.Windows.Controls
+
+[<AbstractClass>]
+type SampleModel() = 
+    inherit Model()
+
+    abstract X : int with get, set
+    abstract Y : int with get, set
+    abstract Result : int with get, set
+
+type SampleEvents = 
+    | Add
+    | Clear
+    | Subtract of int * int
+
+type SimpleView() as this =
+    inherit XamlView<SampleEvents, SampleModel>(resourceLocator = Uri("/Window.xaml", UriKind.Relative))
+
+    let addButton : Button = this ? Add
+    let subtractButton : Button = this ? Subtract
+    let clearButton : Button = this ? Clear
+    let x : TextBox = this ? X
+    let y : TextBox = this ? Y
+    let result : TextBlock = this ? Result
+
+    override this.EventStreams = 
+        [
+            addButton.Click |> Observable.map(fun _ -> Add)
+            clearButton.Click |> Observable.map(fun _ -> Clear)
+            subtractButton.Click |> Observable.map(fun _ -> Subtract(int x.Text, int y.Text))
+        ]
+
+    override this.SetBindings model = 
+        x.SetBinding(TextBox.TextProperty, "X") |> ignore
+        y.SetBinding(TextBox.TextProperty, "Y") |> ignore
+        result.SetBinding(TextBlock.TextProperty, "Result") |> ignore
+
+type SimpleController(view : IView<_, _>) = 
+    inherit Controller<SampleEvents, SampleModel>(view)
+
+    override this.InitModel model = 
+        model.X <- 0
+        model.Y <- 0
+        model.Result <- 0
+
+    override this.EventHandler = function
+        | Add -> this.Add
+        | Clear -> this.InitModel
+        | Subtract(x, y) -> this.Subtract x y
+
+    member this.Add model = 
+        model.Result <- model.X + model.Y
+        
+    member this.Subtract x y model = 
+        model.Result <- x - y
---
+++ Controller/BasicController/SampleControllerTest.fs
@@ -0,0 +1,35 @@
+﻿module Mvc.Wpf.Sample.Test
+
+open System 
+open Xunit
+open Swensen.Unquote.Assertions
+open Mvc.Wpf
+
+let controller = 
+    SimpleController(view = {
+        new IView<SampleEvents, SampleModel> with
+            member __.Subscribe _ = raise <| NotImplementedException()
+            member __.SetBindings _ = raise <| NotImplementedException()
+    })
+
+[<Fact>]
+let InitModel() = 
+    let model = SampleModel.Create()
+    controller.InitModel model
+    test <@ model.X = 0 @>
+    test <@ model.Y = 0 @>
+    test <@ model.Result = 0 @>
+
+[<Fact>]
+let Add() = 
+    let model : SampleModel = Model.Create()
+    model.X <- 3
+    model.Y <- 5
+    controller.EventHandler Add model
+    test <@ model.Result = 8 @>
+
+[<Fact>]
+let Subtract() = 
+    let model = SampleModel.Create()
+    controller.Subtract 11 9 model
+    test <@ model.Result = 2 @>
