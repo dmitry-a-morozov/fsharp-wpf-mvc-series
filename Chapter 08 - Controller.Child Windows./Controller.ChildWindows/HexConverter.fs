4559958e4466d5d5615eba702163f6280651bc46
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-10-05 18:50:31 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@842 aa69542f-e842-d75e-ec41-f0a99e374f1c

--- Chapter 08 - Controller.Child Windows./Controller.ChildWindows/HexConverter.fs
+++ Chapter 08 - Controller.Child Windows./Controller.ChildWindows/HexConverter.fs
@@ -16,19 +16,23 @@ type HexConverterModel() =
             match Int32.TryParse(this.HexValue, NumberStyles.HexNumber, null) with 
             | true, value -> Some(value)
             | false, _ -> None
-        and set(value : int option)= this.HexValue <- value |> Option.get |> sprintf "%X"
+        and set value = 
+            match value with 
+            | Some x -> this.HexValue <- sprintf "%X" x
+            | None -> invalidArg "value" "Value is None."
 
 type HexConverterEvents = 
     | OK
-    | Cancel
 
-type HexConverterView() =
+type HexConverterView() as this =
     inherit View<HexConverterEvents, HexConverterModel, HexConverterWindow>()
+
+    do
+        this.CancelButton <- this.Window.Cancel
 
     override this.EventStreams = 
         [
             this.Window.OK, OK
-            this.Window.Cancel, Cancel
         ]
         |> List.map(fun(button, value) -> button.Click |> Observable.mapTo value)
 
@@ -38,17 +42,15 @@ type HexConverterView() =
                 this.Window.Value.Text <- model.HexValue
             @>
 
-type HexConverterController(view : IView<_, _>) = 
+type HexConverterController(view) = 
     inherit SyncController<HexConverterEvents, HexConverterModel>(view)
 
     override this.InitModel _ = ()
     override this.Dispatcher = function
         | OK -> this.OK
-        | Cancel -> 
-            fun _ -> view.Close false
 
     member this.OK(model : HexConverterModel) = 
         match model.Value with
         | None -> model |> Validation.setError <@ fun m -> m.HexValue @> (sprintf "Cannot parse hex value %s" model.HexValue)
-        | Some _ -> view.Close true
+        | Some _ -> view.OK()
 
