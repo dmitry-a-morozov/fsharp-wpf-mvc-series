9a01703b79d2a2b38ec9742187c723ecedd96901
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-10-07 19:59:15 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@926 aa69542f-e842-d75e-ec41-f0a99e374f1c

---
+++ Chapter 09 - Controller.Re-entrancy Problem/ProblemDemo/Program.fs
@@ -0,0 +1,33 @@
+﻿
+open System
+open System.Windows
+open System.Windows.Controls
+open System.ComponentModel
+open System.Linq
+
+type Model() =
+    let mutable text = ""
+    let propertyChangedEvent = Event<_,_>()
+
+    member this.Text 
+        with get() = text 
+        and set value = 
+            text <- value
+            propertyChangedEvent.Trigger(this, PropertyChangedEventArgs "Text")
+
+    interface INotifyPropertyChanged with
+        [<CLIEvent>]
+        member this.PropertyChanged = propertyChangedEvent.Publish
+
+[<STAThread>] 
+do
+    let model = Model()
+    let textBox = TextBox(DataContext = model)
+    textBox.SetBinding(TextBox.TextProperty, "Text") |> ignore
+    textBox.TextChanged.Add(fun _ ->
+        printfn "Begin event handler. TextBox.Text value: %s. Reverting ..." textBox.Text
+        textBox.Text <- String(textBox.Text.Reverse().ToArray())
+        printfn "End event handler."
+    )
+    model.Text <- "Hello"
+    stdin.ReadLine() |> ignore
