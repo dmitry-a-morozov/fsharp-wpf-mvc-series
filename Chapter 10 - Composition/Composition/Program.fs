bacf872a670e30a271c1454689d4b632666a6c1b
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-10-12 03:01:09 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@1042 aa69542f-e842-d75e-ec41-f0a99e374f1c

--- Chapter 10 - Composition/Composition/Program.fs
+++ Chapter 10 - Composition/Composition/Program.fs
@@ -14,7 +14,7 @@ let main _ =
     let controller = 
         MainController(view, stopWatch)
             .Compose(stopWatchController, stopWatch)
-            <+> (CalculatorController(), CalculatorView(view.Control.Calculator), fun m -> m.Calculator)
+            .Compose(CalculatorController(), CalculatorView(view.Control.Calculator), fun m -> m.Calculator)
             <+> (TempConveterController(), TempConveterView(view.Control.TempConveterControl), fun m -> m.TempConveter)
             <+> (StockPricesChartController(), StockPricesChartView(view.Control.StockPricesChart), fun m -> m.StockPricesChart)
 
