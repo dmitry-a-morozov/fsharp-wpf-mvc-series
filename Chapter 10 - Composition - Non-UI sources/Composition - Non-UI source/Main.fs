c05dcba0ad48b99942a3539a68db2f8e9bdcd177
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-10-14 10:06:28 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@1191 aa69542f-e842-d75e-ec41-f0a99e374f1c

--- Chapter 10 - Composition - Non-UI sources/Composition - Non-UI source/Main.fs
+++ Chapter 10 - Composition - Non-UI sources/Composition - Non-UI source/Main.fs
@@ -1,6 +1,8 @@
 ﻿namespace Mvc.Wpf.Sample
 
 open System
+open System.Diagnostics
+open System.Windows
 open System.Windows.Controls
 open System.Windows.Data
 open Mvc.Wpf
@@ -14,10 +16,13 @@ type MainModel() =
     abstract TempConveter : TempConveterModel with get, set
     abstract StockPricesChart : StockPricesChartModel with get, set
 
+    abstract ProcessName : string with get, set
+    abstract ActiveTab : string with get, set
     abstract RunningTime : TimeSpan with get, set
     abstract Paused : Nullable<bool> with get, set
 
 type MainEvents = 
+    | ActiveTabChanged of string
     | StopWatch
     | StartWatch
     | RestartWatch
@@ -32,9 +37,19 @@ type MainView() as this =
             yield this.Control.RestartWatch.Click |> Observable.mapTo RestartWatch
             yield pause.Checked |> Observable.mapTo StopWatch
             yield pause.Unchecked |> Observable.mapTo StartWatch
+
+            yield this.Control.Tabs.SelectionChanged |> Observable.map(fun _ -> 
+                let activeTab : TabItem = unbox this.Control.Tabs.SelectedItem
+                let header = string activeTab.Header
+                ActiveTabChanged header)
         ]
 
     override this.SetBindings model = 
+        let titleBinding = MultiBinding(StringFormat = "{0} - {1}")
+        titleBinding.Bindings.Add <| Binding("ProcessName")
+        titleBinding.Bindings.Add <| Binding("ActiveTab")
+
+        this.Control.SetBinding(Window.TitleProperty, titleBinding) |> ignore
         Binding.FromExpression <@ this.Control.PauseWatch.IsChecked <- model.Paused @>
         this.Control.RunningTime.SetBinding(TextBlock.TextProperty, Binding(path = "RunningTime", StringFormat = "Running time: {0:hh\:mm\:ss}")) |> ignore
 
@@ -42,6 +57,8 @@ type MainController(view, stopWatch : StopWatchObservable) =
     inherit SupervisingController<MainEvents, MainModel>(view)
 
     override this.InitModel model = 
+        model.ProcessName <- Process.GetCurrentProcess().ProcessName
+        model.ActiveTab <- "Calculator"
         model.RunningTime <- TimeSpan.Zero
         model.Paused <- Nullable false
 
@@ -50,9 +67,13 @@ type MainController(view, stopWatch : StopWatchObservable) =
         model.StockPricesChart <- Model.Create()
 
     override this.Dispatcher = Sync << function
+        | ActiveTabChanged header -> this.ActiveTabChanged header
         | StopWatch -> ignore >> stopWatch.Pause
         | StartWatch -> ignore >> stopWatch.Start
         | RestartWatch -> this.RestartWatch
+
+    member this.ActiveTabChanged header model =
+        model.ActiveTab <- header
 
     member this.RestartWatch model =
         stopWatch.Restart()
