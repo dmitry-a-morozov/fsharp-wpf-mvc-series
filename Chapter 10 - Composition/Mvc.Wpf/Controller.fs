54f49b89332437fac25f4880b347682394602dc9
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-10-14 22:43:45 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@1212 aa69542f-e842-d75e-ec41-f0a99e374f1c

--- Chapter 10 - Composition/Mvc.Wpf/Controller.fs
+++ Chapter 10 - Composition/Mvc.Wpf/Controller.fs
@@ -57,23 +57,23 @@ type SupervisingController<'Event, 'Model when 'Model :> INotifyPropertyChanged>
     abstract OnError : exn -> unit
     default this.OnError why = why |> PreserveStackTraceWrapper |> raise
 
-    member parent.Compose(childController : Controller<'EX, 'MX>, childView : PartialView<'EX, 'MX, _>, modelSelector : 'Model -> 'MX ) = 
+    member this.Compose(childController : Controller<'EX, 'MX>, childView : PartialView<'EX, 'MX, _>, modelSelector : 'Model -> 'MX ) = 
         let compositeView = view.Compose(childView, modelSelector)
         { 
             new SupervisingController<_, _>(compositeView) with
                 member __.InitModel model = 
-                    parent.InitModel model
+                    this.InitModel model
                     childController.InitModel (modelSelector model)
                 member __.Dispatcher = function 
-                    | Choice1Of2 e -> parent.Dispatcher e
+                    | Choice1Of2 e -> this.Dispatcher e
                     | Choice2Of2 e -> 
                         match childController.Dispatcher e with
                         | Sync handler -> Sync(modelSelector >> handler)  
                         | Async handler -> Async(modelSelector >> handler) 
         }
 
-    member parent.Compose(childController : Controller<_, _>, childView : PartialView<_, _, _>) = 
-        parent.Compose(childController, childView, id)
+    member this.Compose(childController : Controller<_, _>, childView : PartialView<_, _, _>) = 
+        this.Compose(childController, childView, id)
 
     static member (<+>) (parent : SupervisingController<_, _>,  (childController, childView, modelSelector)) = 
         parent.Compose(childController, childView, modelSelector)
