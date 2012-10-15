1b0e0888b973e3107b177f25843f4b4d742c2b44
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-10-15 18:00:43 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@1303 aa69542f-e842-d75e-ec41-f0a99e374f1c

---
+++ Chapter 12 - BindingMicroDSL/Mvc.Wpf/Extensions.fs
@@ -0,0 +1,17 @@
+﻿namespace Mvc.Wpf
+ 
+open System
+open LanguagePrimitives
+
+[<AutoOpen>]
+module Extensions = 
+
+    let inline undefined<'T> = raise<'T> <| NotImplementedException()
+
+    let inline positive x = GenericGreaterThan x GenericZero
+
+[<RequireQualifiedAccess>]
+module Observable =
+    let mapTo value = Observable.map(fun _ -> value)
+    let unify first second = Observable.merge (Observable.map Choice1Of2 first) (Observable.map Choice2Of2 second)
+
