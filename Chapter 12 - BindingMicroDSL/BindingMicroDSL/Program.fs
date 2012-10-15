5294246e2e6625115d2a6e1f1fbc6dbaf40457f2
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-10-15 17:58:32 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@1301 aa69542f-e842-d75e-ec41-f0a99e374f1c

---
+++ Chapter 12 - BindingMicroDSL/BindingMicroDSL/Program.fs
@@ -0,0 +1,22 @@
+﻿
+open System
+open System.Windows
+open Mvc.Wpf.Sample
+open Mvc.Wpf
+
+[<STAThread>] 
+do
+    let view = MainView()
+    let stopWatch = StopWatchObservable(frequency = TimeSpan.FromSeconds(1.))
+
+    let stopWatchController(runningTime : TimeSpan) = 
+        Sync <| fun(model : MainModel) -> model.RunningTime <- runningTime
+
+    let controller = 
+        MainController(view, stopWatch)
+            .Compose(stopWatchController, stopWatch)
+            <+> (CalculatorController(), CalculatorView(view.Control.Calculator), fun m -> m.Calculator)
+            <+> (TempConveterController(), TempConveterView(view.Control.TempConveterControl), fun m -> m.TempConveter)
+            <+> (StockPricesChartController(), StockPricesChartView(view.Control.StockPricesChart), fun m -> m.StockPricesChart)
+
+    controller |> Controller.start |> ignore
---
+++ Chapter 12 - BindingMicroDSL/BindingMicroDSL/StockPicker.fs
@@ -0,0 +1,65 @@
+﻿namespace Mvc.Wpf.Sample
+
+open System
+open System.Globalization
+open System.Windows.Data
+open System.Windows.Controls
+open System.Net
+open System.Linq
+open Mvc.Wpf
+open Mvc.Wpf.UIElements
+
+[<AbstractClass>]
+type StockPickerModel() = 
+    inherit Model()
+
+    abstract Symbol : string with get, set
+    abstract CompanyName : string with get, set
+    abstract LastPrice : decimal with get, set
+    abstract AddToChartEnabled : bool with get, set
+
+type StockPickerView() as this =
+    inherit View<unit, StockPickerModel, StockPickerWindow>()
+
+    do
+        this.Control.Symbol.CharacterCasing <- CharacterCasing.Upper
+        this.CancelButton <- this.Control.CloseButton
+        this.DefaultOKButton <- this.Control.AddToChart
+
+    override this.EventStreams = 
+        [
+            this.Control.Retrieve.Click |> Observable.mapTo()
+        ]
+
+    override this.SetBindings model = 
+        Binding.FromExpression 
+            <@ 
+                this.Control.Symbol.Text <- model.Symbol
+                this.Control.CompanyName.Text <- model.CompanyName
+                this.Control.LastPrice.Text <- string model.LastPrice 
+                this.Control.AddToChart.IsEnabled <- model.AddToChartEnabled
+            @>
+
+type StockPickerController(view) = 
+    inherit SupervisingController<unit, StockPickerModel>(view)
+
+    override this.InitModel _ = ()
+    override this.Dispatcher = fun() ->
+        Async <| fun model ->
+            async {
+                model |> Validation.textRequired <@ fun m -> m.Symbol @>
+                if not model.HasErrors 
+                then 
+                    use wc = new WebClient()
+                    let url = Uri(sprintf "http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=nl1" model.Symbol)
+                    let! data = wc.AsyncDownloadString url
+                    match data.Split([| Environment.NewLine |], StringSplitOptions.RemoveEmptyEntries).Single().Split(',') with
+                    | [| n; "0.00" |] when n = sprintf "\"%s\"" model.Symbol -> 
+                        model |> Validation.setError <@ fun m -> m.Symbol @> "Invalid security symbol."
+                    | [| n; l1 |] -> 
+                        model.CompanyName <- n
+                        model.LastPrice <- decimal l1
+                        model.AddToChartEnabled <- true
+                    | _ -> failwithf "Unexpected result service call result.\nRequest: %O.\nResponse: %s" url data
+            }
+
