c1537fb4d6c616acee0897211aef679106ffb597
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-10-16 12:07:17 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@1305 aa69542f-e842-d75e-ec41-f0a99e374f1c

--- Chapter 12 - BindingMicroDSL/Mvc.Wpf/Binding.fs
+++ Chapter 12 - BindingMicroDSL/Mvc.Wpf/Binding.fs
@@ -23,7 +23,7 @@ let rec (|PropertyPath|_|) = function
     | _ -> None
 
 type Expr with
-    member this.ToBindingExpr() = 
+    member this.ToBindingExpr(?mode, ?updateSourceTrigger, ?fallbackValue, ?targetNullValue, ?validatesOnDataErrors) = 
         match this with
         | PropertySet
             (
@@ -33,18 +33,28 @@ type Expr with
                 PropertyPath path
             ) ->
                 let target : FrameworkElement = (view, [||]) |> window.GetValue |> control.GetValue |> unbox
-                let binding = Binding(path, ValidatesOnDataErrors = true)
+
+                let binding = Binding(path)
+                binding.ValidatesOnDataErrors <- defaultArg validatesOnDataErrors true
+                if mode.IsSome then binding.Mode <- mode.Value
+                if updateSourceTrigger.IsSome then binding.UpdateSourceTrigger <- updateSourceTrigger.Value
+                if fallbackValue.IsSome then binding.FallbackValue <- fallbackValue.Value
+                if targetNullValue.IsSome then binding.TargetNullValue <- targetNullValue.Value
+
                 target.SetBinding(targetProperty.DependencyProperty, binding)
         | _ -> invalidArg "expr" (string this) 
 
 type Binding with
-    static member FromExpression expr = 
+    static member FromExpression(expr : Expr, ?mode, ?updateSourceTrigger, ?fallbackValue, ?targetNullValue, ?validatesOnDataErrors) =
         let rec split = function 
             | Sequential(head, tail) -> head :: split tail
             | tail -> [ tail ]
 
         for e in split expr do
-            let be = e.ToBindingExpr()
+            let be = e.ToBindingExpr(?mode = mode, ?updateSourceTrigger = updateSourceTrigger, ?fallbackValue = fallbackValue, ?targetNullValue = targetNullValue, ?validatesOnDataErrors = validatesOnDataErrors)
             assert not be.HasError
     
+    static member TwoWay(expr : Expr) = Binding.FromExpression(expr, BindingMode.TwoWay)
+    static member OneWay(expr : Expr) = Binding.FromExpression(expr, BindingMode.OneWay)
+    static member UpdateSourceOnChange(expr : Expr) = Binding.FromExpression(expr, updateSourceTrigger = UpdateSourceTrigger.PropertyChanged)
 
