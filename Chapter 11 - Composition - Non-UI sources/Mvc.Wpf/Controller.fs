1d085a8bbe690d3c8b5be369299b00aa57bd6d5b
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-10-17 15:39:00 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@1335 aa69542f-e842-d75e-ec41-f0a99e374f1c

--- Chapter 11 - Composition - Non-UI sources/Mvc.Wpf/Controller.fs
+++ Chapter 11 - Composition - Non-UI sources/Mvc.Wpf/Controller.fs
@@ -38,11 +38,11 @@ type SupervisingController<'Event, 'Model when 'Model :> INotifyPropertyChanged>
 #if DEBUG
         let observer = observer.Checked()
 #endif
-        let observer = Observer.Synchronize(observer, preventReentrancy = true)
+        let nonReentrantObserver = Observer.Synchronize(observer, preventReentrancy = true)
 
         let scheduler = SynchronizationContextScheduler(SynchronizationContext.Current, alwaysPost = false)
         view.ObserveOn(scheduler)
-            .Subscribe(observer)
+            .Subscribe(nonReentrantObserver)
 
     member this.Start model =
         use subcription = this.Activate model
