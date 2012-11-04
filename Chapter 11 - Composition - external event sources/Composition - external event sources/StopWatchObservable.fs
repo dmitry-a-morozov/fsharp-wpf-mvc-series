421c772f7819b581d130cf1469ddf3e571efde00
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-11-04 22:56:22 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@1711 aa69542f-e842-d75e-ec41-f0a99e374f1c

--- Chapter 11 - Composition - external event sources/Composition - external event sources/StopWatchObservable.fs
+++ Chapter 11 - Composition - external event sources/Composition - external event sources/StopWatchObservable.fs
@@ -7,7 +7,7 @@ open System.Reactive.Linq
 type StopWatchObservable(frequency, failureFrequencyInSeconds) =
     let watch = Stopwatch.StartNew()
     let paused = ref false
-    let generareFailures = ref false
+    let generateFailures = ref false
 
     member this.Pause() = 
         watch.Stop()
@@ -19,14 +19,14 @@ type StopWatchObservable(frequency, failureFrequencyInSeconds) =
         watch.Restart()
         paused := false
 
-    member this.GenerareFailures with set value = generareFailures := value
+    member this.GenerateFailures with set value = generateFailures := value
 
     interface IObservable<TimeSpan> with
         member this.Subscribe observer = 
             Observable.Interval(period = frequency)
                 .Where(fun _ -> not !paused)
                 .Select(fun _ -> 
-                    if !generareFailures && watch.Elapsed.TotalSeconds % failureFrequencyInSeconds < 1.0
+                    if !generateFailures && watch.Elapsed.TotalSeconds % failureFrequencyInSeconds < 1.0
                     then failwithf "failing every %.1f secs" failureFrequencyInSeconds
                     else watch.Elapsed)
                 .Subscribe(observer)
