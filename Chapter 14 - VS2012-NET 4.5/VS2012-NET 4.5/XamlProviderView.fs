b1798f8de43a3568b76e7259b31ef692a2b7effe
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-11-07 16:47:29 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@1746 aa69542f-e842-d75e-ec41-f0a99e374f1c

---
+++ Chapter 14 - VS2012-NET 4.5/VS2012-NET 4.5/XamlProviderView.fs
@@ -0,0 +1,41 @@
+﻿module Mvc.Wpf.Sample
+
+open System
+open FSharpx.TypeProviders.XamlProvider
+
+[<AbstractClass>]
+type XamlProviderPartialView<'Event, 'Model>(xaml: #XamlFile) =
+
+    member this.XamlFile = xaml
+    
+    interface IPartialView<'Event, 'Model> with
+        member this.Subscribe observer = 
+            let xs = this.EventStreams |> List.reduce Observable.merge 
+            xs.Subscribe observer
+        member this.SetBindings model = 
+            xaml.Root.DataContext <- model; 
+            this.SetBindings model
+
+    abstract EventStreams : IObservable<'Event> list
+    abstract SetBindings : 'Model -> unit
+
+[<AbstractClass>]
+type View<'Event, 'Model>(xaml) = 
+    inherit XamlProviderPartialView<'Event, 'Model>(xaml)
+
+    let mutable isOK = false
+
+    interface IView<'Event, 'Model> with
+        member this.ShowDialog() = 
+            this.XamlFile.Root.ShowDialog() |> ignore
+            isOK
+        member this.Show() = 
+            this.Control.Show()
+            this.Control.Closed |> Event.map (fun _ -> isOK) |> Async.AwaitEvent 
+        member this.Close isOK' = 
+            isOK <- isOK'
+            this.Control.Close()
+
+
+
+
