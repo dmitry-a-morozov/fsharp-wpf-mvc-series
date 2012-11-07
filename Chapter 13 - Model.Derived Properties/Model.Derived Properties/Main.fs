2da99cf2c8cdc8c36126448731d90019b37a36ce
dmitry.a.morozov@gmail.com <dmitry.a.morozov@gmail.com@aa69542f-e842-d75e-ec41-f0a99e374f1c>
2012-11-07 23:10:33 +0000

    git-svn-id: http://fsharpmvcseries.googlecode.com/svn/trunk@1748 aa69542f-e842-d75e-ec41-f0a99e374f1c

--- Chapter 13 - Model.Derived Properties/Model.Derived Properties/Main.fs
+++ Chapter 13 - Model.Derived Properties/Model.Derived Properties/Main.fs
@@ -82,4 +82,3 @@ type MainController(view, stopWatch : StopWatchObservable) =
         stopWatch.Restart()
         model.Paused <- false
 
-    override this.OnError why = Debug.WriteLine why.Message
