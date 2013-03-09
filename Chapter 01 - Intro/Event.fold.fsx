3f8f5d5dfc667c1fa2d860bb8e9a7c4f63ec6571
Dmitry <dmitry.a.morozov@gmail.com>
2013-03-09 10:48:45 -0500

    Intro chapter

---
+++ Chapter 01 - Intro/Event.fold.fsx
@@ -0,0 +1,21 @@
+﻿
+type Events = 
+    | Add of int 
+    | Subtract of int
+
+type View = IEvent<Events>  
+
+type Model = { State : int }
+
+type Controller = (Events -> Model -> Model) -> Model -> IEvent<Events> -> Model
+
+
+
+open Unchecked
+let view = defaultof<View>
+let model = defaultof<Model>
+let eventHandler = defaultof<Events -> Model -> Model>
+let controller = defaultof<Controller>
+
+let result = view |> controller eventHandler model
+
---
+++ Chapter 01 - Intro/IObservable.Mutation.fsx
@@ -0,0 +1,33 @@
+﻿
+open System
+
+type Events = 
+    | Add of int 
+    | Subtract of int
+
+type View = IObservable<Events>  
+
+type Model = { mutable State : int }
+
+type EventHandler = Events -> Model -> unit
+type Mvc = EventHandler -> Model -> IObservable<Events> -> IDisposable
+
+
+
+let subject = new Event<Events>()
+let raiseEvents() = [Add 2; Subtract 1; Add 5] |> List.iter subject.Trigger
+
+let view = subject.Publish
+let model : Model = { State = 6 }
+let eventHandler event model = 
+    match event with
+    | Add x -> model.State <- model.State + x 
+    | Subtract x -> model.State <- model.State - x 
+
+let mvc : Mvc = fun eventhandler model view -> view.Subscribe(fun event -> eventHandler event model; printfn "Model: %A" model)
+
+let subscription = view |> mvc eventHandler model
+
+raiseEvents()
+
+subscription.Dispose()
