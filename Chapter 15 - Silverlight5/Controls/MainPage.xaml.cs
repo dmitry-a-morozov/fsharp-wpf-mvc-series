4b937d8e740fcb8df82aa2eec7e36c6fe88f58e0
Dmitry <dmitry.a.morozov@gmail.com>
2013-03-12 00:33:31 -0400

    continure on SL host

--- Chapter 15 - Silverlight5/Controls/MainPage.xaml.cs
+++ Chapter 15 - Silverlight5/Controls/MainPage.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
-namespace FSharp.Windows.Sample
+namespace SampleApp
 {
     public partial class MainPage : UserControl
     {
